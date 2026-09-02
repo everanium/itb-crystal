d110cb313d58ba0ae9422dc469183a290ce7924f
Andrey Kuvshinov <andrew@encloud.blue>
2026-09-03 00:35:36 +0700

    sync from monorepo e13ae3a
    Source: github.com/everanium/itb @ e13ae3acf4d4f3dad4b0b05b4fe97eec2f93e869

--- src/itb.cr
+++ src/itb.cr
@@ -24,7 +24,7 @@ require "./itb/stream"
 
 module ITB
   # Binding version (matches shard.yml).
-  VERSION = "0.3.1"
+  VERSION = "0.3.2"
 
   # Shipped profile identifiers, mirrored from the Go triple package
   # registry in declaration order. The C ABI exposes no profile
